9c8a54f25b65dcfd22a0a3b25271649467108df9
20211011110001 <20211011110001@CNAT407150>
2022-12-23 17:42:07 -0300

    Adicionar arquivos de projeto.

---
+++ EscolaApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EscolaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Turma_click(object sender, RoutedEventArgs e)
+        {
+            Turmajanela TJ = new Turmajanela();
+            TJ.ShowDialog();
+        }
+
+        private void Aluno_click(object sender, RoutedEventArgs e)
+        {
+            AlunoJanela AJ = new AlunoJanela();
+            AJ.ShowDialog();
+        }
+
+        private void Matricula_click(object sender, RoutedEventArgs e)
+        {
+            MatriculaJanela MJ = new MatriculaJanela();
+            MJ.ShowDialog();
+        }
+
+        private void Diario_click(object sender, RoutedEventArgs e)
+        {
+            DiarioJanela DJ = new DiarioJanela();
+            DJ.ShowDialog();
+        }
+    }
+}
---
+++ EscolaApp/nTurma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaApp
+{
+    static class nTurma
+    {
+        private static List<Turma> Turmas = new List<Turma>();
+        public static void Inserir(Turma t)
+        {
+            Turmas.Add(t);
+        }
+        public static List<Turma> Listar()
+        {
+            return Turmas;
+        }
+    }
+}
